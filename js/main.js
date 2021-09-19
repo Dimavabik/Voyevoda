838de1763bd87e0ebc8eac0d0d42c8c3178e7c09
Maria <artes30@mail.ru>
2021-09-19 14:55:40 +0300

    added winow.onload

--- js/main.js
+++ js/main.js
@@ -1,37 +1,39 @@
-let animItems = document.getElementsByClassName('animateItems');
+window.onload = function () {
+    let animItems = document.getElementsByClassName('animateItems');
 
 
-if (animItems.length > 0) {
-    window.addEventListener('scroll', animOnScroll)
+    if (animItems.length > 0) {
+        window.addEventListener('scroll', animOnScroll)
 
-    function animOnScroll() {
-        for (let i = 0; i < animItems.length; i++) {
-            let animItem = animItems[i];
-            let animItemHeight = animItem.offsetHeight;
-            let animItemOffset = offset(animItem).top;
-            let animStart = 4;
+        function animOnScroll() {
+            for (let i = 0; i < animItems.length; i++) {
+                let animItem = animItems[i];
+                let animItemHeight = animItem.offsetHeight;
+                let animItemOffset = offset(animItem).top;
+                let animStart = 4;
 
-            let animItemPoint = window.innerHeight - animItemHeight / animStart;
-            if (animItemHeight > window.innerHeight) {
-                animItemPoint = window.innerHeight - window.innerHeight / animStart;
-            }
-            if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
-                animItem.classList.add('_active')
-            } else {
-                animItem.classList.remove('_active')
+                let animItemPoint = window.innerHeight - animItemHeight / animStart;
+                if (animItemHeight > window.innerHeight) {
+                    animItemPoint = window.innerHeight - window.innerHeight / animStart;
+                }
+                if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+                    animItem.classList.add('_active')
+                } else {
+                    animItem.classList.remove('_active')
+                }
             }
         }
+
+        function offset(el) {
+            const rect = el.getBoundingClientRect(),
+                scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+                scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            return {top: rect.top + scrollTop, left: rect.left + scrollLeft}
+        }
+
+        setTimeout(() => {
+            animOnScroll()
+        }, 300);
     }
 
-    function offset(el) {
-        const rect = el.getBoundingClientRect(),
-            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-        return {top: rect.top + scrollTop, left: rect.left + scrollLeft}
-    }
-
-    setTimeout(() => {
-        animOnScroll()
-    }, 300);
 }
-
